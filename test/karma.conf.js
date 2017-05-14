4a1901fd64cab3dc0789f8e300610ce8e942970c
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-14 18:09:17 -0300

    Final attempt 3

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -20,8 +20,8 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'app/bower_components/angular/angular.js',
       'app/bower_components/jquery/dist/jquery.js',
+      'app/bower_components/angular/angular.js',
       'app/bower_components/bootstrap/dist/js/bootstrap.js',
       'app/bower_components/angular-animate/angular-animate.js',
       'app/bower_components/angular-aria/angular-aria.js',
