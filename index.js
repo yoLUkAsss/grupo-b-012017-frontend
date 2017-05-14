3751994582c2e00b826a8d02b51bb0a096e25aaa
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-13 21:07:05 -0300

    Fixing the wrong use of html views

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('views/index.html');
