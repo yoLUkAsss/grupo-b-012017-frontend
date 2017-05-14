9462ea86ac8f5df7fde58fef532750a2df2620aa
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-14 13:05:56 -0300

    Final Attempt

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alolocoAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the alolocoAppApp
+ */
+angular.module('alolocoAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alolocoAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the alolocoAppApp
+ */
+angular.module('alolocoAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
